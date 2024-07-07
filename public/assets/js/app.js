5178ae7333475617060af72deb77fc91ba390cfd
xaricipul <xaricipul@gmail.com>
2024-07-07 19:12:47 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,7 +11,7 @@ let messagesQueue = [];
 $(document).ready(() => {
 
     setTimeout(function () {
-        let targetLive = "alisa.macheer";
+        let targetLive = "mameemacheer";
         connect(targetLive);
     }, 5000);
 
